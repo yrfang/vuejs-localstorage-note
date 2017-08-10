accf29b0a765991f3d705f38e559cc18ce31d65a
yrfang <fyr1116@gmail.com>
2017-08-10 17:19:59 +0800

    title

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Listpage from '@/components/Listpage';
+import NoteList from '@/components/NoteList';
 
 Vue.use(Router);
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Listpage',
-      component: Listpage
+      name: 'note',
+      component: NoteList
     }
   ]
 })
