3b0323bf98b1e716e0b92ca76b4b2ee4156800d3
yrfang <fyr1116@gmail.com>
2017-08-10 22:01:50 +0800

    scroll of notes

--- src/App.vue
+++ src/App.vue
@@ -17,8 +17,6 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-  /*margin-top: 60px;*/
-  padding: 40px;
 }
 
 .row {
--- src/components/NoteList.vue
+++ src/components/NoteList.vue
@@ -1,11 +1,13 @@
 <template lang="pug">
 .NoteList
-  .row
-    h3 Keep Your Notes with Local Storage
-  .row.infoBar
-    i.fa.fa-search
-    input.form-control.search(placeholder="search your notes")
-    button.addNote Add Note
+  .topBar
+    .row.searchBar
+      i.fa.fa-search
+      input.form-control.search(placeholder="search your notes")
+    .row
+      select.tagSelect
+        option(v-for="i in 5") {{i}}
+      button.addNote Add Note
   .row.NoteListItem
     .noteContainer(v-for="note in notes")
       .topper
@@ -32,8 +34,8 @@ export default {
         { title: 'note title 1', meta: 'Added 20 days ago', text: 'the example from Bootstrapping', tag: 'work' },
         { title: 'note title 2', meta: 'Added 15 days ago', text: 'the example from Bootstrapping', tag: 'family' },
         { title: 'And another one', meta: 'Added 10 days ago', text: 'the example', tag: 'family' },
-        { title: 'And another 3', meta: 'Added 3 days ago' , text: 'hi', tag: ''},
-        {title: 'the example from Bootstrapping', meta: 'created and learned on 2017/07/25'}
+        {title: 'the example from Bootstrapping', meta: 'created and learned on 2017/07/25'},
+        { title: 'And another one', meta: 'Added 10 days ago', text: 'the example', tag: 'family' }
       ];
       // console.log(testData);
       this.notes = testData;
@@ -46,26 +48,46 @@ export default {
 h3
   font-weight: bold
 
-.infoBar
+.NoteList
   position: relative
 
-  i.fa.fa-search
-    position: absolute
-    top: 10px
-    left: 15px
-    font-size: 24px
-    font-weight: 100
-    color: rgba(#000, 0.2)
+.topBar
+  padding: 20px 30px
+  margin-bottom: 30px
+  position: fixed
+  top: 0px
+  left: 0px
+  width: 100%
+  background-color: #eee
 
-  input.search
-    padding-top: 10px
-    padding-bottom: 10px
-    padding-left: 50px
-    margin-right: 15px
-    margin-bottom: 10px
-    font-size: 18px
-    width: 90%
+  .searchBar
+    position: relative
+
+    i.fa.fa-search
+      position: absolute
+      top: 10px
+      left: 20px
+      font-size: 24px
+      font-weight: 100
+      color: rgba(#000, 0.2)
+
+    input.search
+      padding-top: 10px
+      padding-bottom: 10px
+      padding-left: 50px
+      margin-right: 15px
+      margin-bottom: 10px
+      font-size: 18px
+      width: 100%
+      height: 45px
+
+  select.tagSelect
+    width: 20%
     height: 45px
+    margin-right: 15px
+    padding: 5px 10px
+    background-color: #fff
+    border: solid 1px rgba(#000, 0.2)
 
   button.addNote
     border: none
@@ -84,6 +106,14 @@ h3
 .NoteListItem
   padding-left: 100px
   padding-right: 100px
+  position: fixed
+  top: 200px
+  left: 0px
+  right: 0px
+  bottom: 20px
+  width: 100%
+  overflow-y: scroll
+  z-index: -10
 
 .noteContainer
   border: solid 1px #bbb
@@ -91,7 +121,9 @@ h3
   padding: 20px
   letter-spacing: 1.5px
   margin: 10px
-  width: 100%
+  margin-left: auto
+  margin-right: auto
+  width: 80%
   height: 150px
 
   .topper
